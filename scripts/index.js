d4465ac884327e1eecc2afaa05f24c0bd6b2b8dd
Felix Manuel <luxotus@gmail.com>
2018-08-31 20:55:19 -0400

    Basic rotating green cube

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,33 @@
-console.log('test');
+/* global THREE */
+
+// Creating the scene
+const scene = new THREE.Scene();
+const fieldOfView = 75;
+const aspectRatio = window.innerWidth / window.innerHeight;
+const near = 0.1;
+const far = 1000;
+const camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, near, far);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Creating a cube
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+// Rendering the scene
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
